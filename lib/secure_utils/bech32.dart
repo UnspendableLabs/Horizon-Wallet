518151c3869ad8efb208382099c691ec356ad242
dime-squanderer <159725482+dime-squanderer@users.noreply.github.com>
2024-03-16 12:18:03 -0400

    successfully recover freewallet

--- lib/secure_utils/bech32.dart
+++ lib/secure_utils/bech32.dart
@@ -10,30 +10,43 @@ class Bech32Address {
     var sha256 = pointycastle.Digest("SHA-256");
     var publicKeySha256 = sha256.process(publicKey);
 
-    // Step 2: Hash the result of step 1 using RIPEMD-160
     var ripemd160 = pointycastle.Digest('RIPEMD-160');
     var publicKeyRipemd160 = ripemd160.process(publicKeySha256);
+    List<int> ripemd5bit = to5bitArray(Uint8List.fromList(publicKeyRipemd160));
 
-// Step 3: Prepend the network identifier byte
     List<int> version = [_getVersion()]; // Assuming Bitcoin network
-    List<int> data = [...version, ...publicKeyRipemd160];
-    Uint8List uint8ListData = Uint8List.fromList(data);
+    List<int> data = [...version, ...ripemd5bit];
 
-// Step 4: Hash the result of step 3 twice using SHA-256 and take the first four bytes as the checksum
-    var hash1 = sha256.process(uint8ListData);
-    var hash2 = sha256.process(hash1);
-    var checksum = hash2.sublist(0, 4);
-
-    // Step 5: Append the checksum to the result of step 3
-    List<int> payload = data + checksum;
-
-// Step 6: Encode the result using Bech32 encoding
     String hrp = _getHrp();
-    Bech32 bech32 = Bech32(hrp, payload);
+    Bech32 bech32 = Bech32(hrp, data);
+    print('BECH32: ${bech32.data}');
     var encoder = const Bech32Codec();
     String bech32Address = encoder.encode(bech32);
 
     return bech32Address;
+  }
+
+  List<int> to5bitArray(Uint8List data) {
+    List<int> bits5Array = [];
+    int buffer = 0;
+    int bitsFilled = 0;
+
+    for (int byte in data) {
+      buffer = (buffer << 8) | byte;
+      bitsFilled += 8;
+
+      while (bitsFilled >= 5) {
+        bitsFilled -= 5;
+        bits5Array.add((buffer >> bitsFilled) & 31); // Extract the top 5 bits
+      }
+    }
+
+    if (bitsFilled > 0) {
+      // Add the remaining bits as a 5-bit value (padded with zeros if necessary)
+      bits5Array.add((buffer << (5 - bitsFilled)) & 31);
+    }
+
+    return bits5Array;
   }
 
   _getHrp() {
--- test/wallet_recovery/freewallet_recovery/freewallet_recovery.test.dart
+++ test/wallet_recovery/freewallet_recovery/freewallet_recovery.test.dart
@@ -5,18 +5,50 @@ import 'package:test/test.dart';
 
 void main() async {
   await dotenv.load();
+  String findAddress(String targetAddress, List<String> addresses) =>
+      addresses.firstWhere((address) => address == targetAddress);
 
   group('FreewalletRecovery', () {
     var freewalletRecovery = FreewalletRecovery();
 
-    test('recoverFreewallet', () {
-      const List<String> expectedAddress = [];
-      String mnemonic = "threaten study pierce annoy leave wrote box drift drove soldier toss gasp";
+    test('recoverFreewallet bip39', () {
+      const List<String> expectedAddresses = [
+        "18yGaEy48a3PPcg33e7xj3LSKA8pCTbAMh",
+        "bc1q2akjey7dppxqjuv9amvjnfpp5xtm5uguxquql0",
+        "1NaidkrBeCMysjGj8d5xyFtGY1ZqPxjJFb",
+        "bc1qaja37nwsrfp0mp3u6vrm7qx6j05rzt3ucg8cag",
+        "18uu4keUgrH54SS1JN8BRF3YxoeG6NMjtw",
+        "bc1q2myl3n7nc7nxsfn5h3ge34jl4mdu3wax876xwd",
+        "14ZcDQs3Bs4DEvqMcjnaCWihWPmhmNarW8",
+        "bc1qyuf325j0tw05ek6rpm0t0lgq66vqcsq38y4jyk",
+        "1PB3rRptP6Wg5m6BsV7i4kpCYdCK9bDYRX",
+        "bc1q7vu5ajvup388rqltveprrpuagkk7kjz6lm3cuf",
+        "1GkE7L6eBWTUWZLo4fWseUiRca8ARHztPn",
+        "bc1q4jmtwnaenl5tl5mf744t9zk89pd6ncd4kva678",
+        "1HBNfdbY3L3BCPao54YDMfbtQ2vGsSjzUr",
+        "bc1qk9uyyy9thkhtl2vn2cm367p9fd5rdv6sh0ll06",
+        "1KoWYtvDVCbGb4GanGxsQ7jDJcWhwMXGUB",
+        "bc1qeclpp0yuwwdvgtjsd3h8wnj7fcnaevlnaf4pwa",
+        "15eYhZAqtZ2Mo7io14eS44SaCTTnvupx8Y",
+        "bc1qxta94knhpkyp2mck6l4sy0f9lhw5tnkg5nhe0m",
+        "1GtzK7W8xyrsj7wnD8PXU8WYt6X57voWy5",
+        "bc1q4e00z4qp56cfngj9jp7ra7knw2lxu3u8ry7gfa"
+      ];
+      String mnemonic =
+          "silver similar slab poet cannon south antique finish large romance climb faculty";
 
       List<WalletInfo> wallets = freewalletRecovery.recoverFreewallet(mnemonic);
+
       for (var wallet in wallets) {
-        expect(expectedAddress.contains(wallet.address), true);
+        print(wallet.address);
+        expect(expectedAddresses.contains(wallet.address), true);
       }
+      // expect(wallets.length, expectedAddresses.length);
+      // for (int i = 0; i < wallets.length; i++) {
+      //   WalletInfo wallet = wallets[i];
+      //   // String address = findAddress(wallet.address, expectedAddresses);
+      //   print('ADDRESS: ${wallet.address}');
+      // }
     });
   });
 }
