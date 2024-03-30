68336fe236ee2560a1a4aad35b8711e81d946599
dime-squanderer <159725482+dime-squanderer@users.noreply.github.com>
2024-03-29 20:16:30 -0400

    remove unused loading

--- lib/redux/models/wallet_retrieve_info_state.dart
+++ lib/redux/models/wallet_retrieve_info_state.dart
@@ -2,17 +2,15 @@ import 'package:meta/meta.dart';
 
 @immutable
 class WalletRetrieveInfoState {
-  final bool? isLoading;
   final String? seedHex;
   final String? walletType;
 
   const WalletRetrieveInfoState({
-    this.isLoading,
     @required this.seedHex,
     @required this.walletType,
   });
 
   factory WalletRetrieveInfoState.initial() {
-    return const WalletRetrieveInfoState(isLoading: false, seedHex: null, walletType: null);
+    return const WalletRetrieveInfoState( seedHex: null, walletType: null);
   }
 }
--- lib/redux/models/wallet_retrieve_info_view.dart
+++ lib/redux/models/wallet_retrieve_info_view.dart
@@ -3,7 +3,6 @@ import 'package:uniparty/redux/app_store.dart';
 import 'package:uniparty/redux/middleware/secure_storage_thunk_middleware.dart';
 
 class WalletRetrieveInfoViewModel {
-  final bool? isLoading;
   final String? seedHex;
   final String? walletType;
 
@@ -11,15 +10,10 @@ class WalletRetrieveInfoViewModel {
   final Function(String, String) saveToState;
 
   WalletRetrieveInfoViewModel(
-      {required this.isLoading,
-      this.seedHex,
-      this.walletType,
-      required this.saveInfo,
-      required this.saveToState});
+      {this.seedHex, this.walletType, required this.saveInfo, required this.saveToState});
 
   static WalletRetrieveInfoViewModel fromStore(Store<AppState> store) {
     return WalletRetrieveInfoViewModel(
-        isLoading: store.state.walletRetrieveInfoState.isLoading,
         seedHex: store.state.walletRetrieveInfoState.seedHex,
         walletType: store.state.walletRetrieveInfoState.walletType,
         saveInfo: (String seedHex, String walletType) async {
