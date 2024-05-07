d7ea6045e84eb52014e834e2a527b20bee205707
matt marcello <matt.marcello@gmail.com>
2024-05-07 11:58:16 -0400

    wip

--- lib/models/bitcoinjs_transaction.dart
+++ lib/models/bitcoinjs_transaction.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:uniparty/models/buffer_reader.dart';
+import 'package:hex/hex.dart';
 
 class BitcoinjsTransaction {
   int version;
@@ -14,16 +15,64 @@ class BitcoinjsTransaction {
   bool checkWitnesses() {
     return ins.any((input) => input.witness.isNotEmpty);
   }
+
+  factory BitcoinjsTransaction.fromHex(String txHex) {
+
+    final buffer = Uint8List.fromList( HEX.decode(txHex));
+    final reader = BufferReader(buffer);
+    final tx = BitcoinjsTransaction();
+    tx.version = reader.readInt32();
+    final marker = reader.readUInt8();
+    final flag = reader.readUInt8();
+
+    if (marker == 0 && flag == 1) {
+      tx.hasWitnesses = true;
+    } else {
+      reader.offset -= 2; // Rewind the offset if it's not a witness transaction
+    }
+
+    var vinLen = reader.readVarInt();
+    for (int i = 0; i < vinLen; i++) {
+      tx.ins.add(TxInput(
+        hash: HEX.encode(reader.readSlice(32).reversed.toList()),
+        index: reader.readUInt32(),
+        script: HEX.encode(reader.readSlice(reader.readVarInt())),
+        sequence: reader.readUInt32(),
+      ));
+    }
+
+    var voutLen = reader.readVarInt();
+    for (int i = 0; i < voutLen; i++) {
+      tx.outs.add(TxOutput(
+        value: reader.readUInt64().toInt(),
+        script: reader.readSlice(reader.readVarInt()),
+      ));
+    }
+
+    if (tx.hasWitnesses) {
+      for (var input in tx.ins) {
+        input.witness = reader.readVector();
+      }
+    }
+
+    tx.locktime = reader.readUInt32();
+    return tx;
+  }
 }
 
 class TxInput {
-  Uint8List hash;
+  String hash;
   int index;
-  Uint8List script;
+  String script;
   int sequence;
   List<Uint8List> witness;
 
-  TxInput({required this.hash, required this.index, required this.script, required this.sequence, this.witness = const []});
+  TxInput(
+      {required this.hash,
+      required this.index,
+      required this.script,
+      required this.sequence,
+      this.witness = const []});
 }
 
 class TxOutput {
@@ -31,45 +80,4 @@ class TxOutput {
   Uint8List script;
 
   TxOutput({required this.value, required this.script});
-}
-
-BitcoinjsTransaction parseTransaction(Uint8List buffer) {
-  final reader = BufferReader(buffer);
-  final tx = BitcoinjsTransaction();
-  tx.version = reader.readInt32();
-  final marker = reader.readUInt8();
-  final flag = reader.readUInt8();
-
-  if (marker == 0 && flag == 1) {
-    tx.hasWitnesses = true;
-  } else {
-    reader.offset -= 2; // Rewind the offset if it's not a witness transaction
-  }
-
-  var vinLen = reader.readVarInt();
-  for (int i = 0; i < vinLen; i++) {
-    tx.ins.add(TxInput(
-      hash: reader.readSlice(32),
-      index: reader.readUInt32(),
-      script: reader.readSlice(reader.readVarInt()),
-      sequence: reader.readUInt32(),
-    ));
-  }
-
-  var voutLen = reader.readVarInt();
-  for (int i = 0; i < voutLen; i++) {
-    tx.outs.add(TxOutput(
-      value: reader.readUInt64().toInt(),
-      script: reader.readSlice(reader.readVarInt()),
-    ));
-  }
-
-  if (tx.hasWitnesses) {
-    for (var input in tx.ins) {
-      input.witness = reader.readVector();
-    }
-  }
-
-  tx.locktime = reader.readUInt32();
-  return tx;
 }
---
+++ test/transaction/transaction_test.dart
@@ -0,0 +1,68 @@
+import 'package:test/test.dart';
+import 'package:uniparty/models/bitcoinjs_transaction.dart';
+
+// class BitcoinjsTransaction {
+//   int version;
+//   List<TxInput> ins = [];
+//   List<TxOutput> outs = [];
+//   int locktime;
+//   bool hasWitnesses = false;
+// BitcoinjsTransaction({this.version = 1, this.locktime = 0});
+//
+//   bool checkWitnesses() {
+//     return ins.any((input) => input.witness.isNotEmpty);
+//   }
+// }
+//
+// class TxInput {
+//   Uint8List hash;
+//   int index;
+//   Uint8List script;
+//   int sequence;
+//   List<Uint8List> witness;
+//
+//   TxInput({required this.hash, required this.index, required this.script, required this.sequence, this.witness = const []});
+// }
+//
+// class TxOutput {
+//   int value;
+//   Uint8List script;
+//
+//   TxOutput({required this.value, required this.script});
+// }
+
+void main() async {
+  group('legacy', () {
+    test('parsed unsigned', () {
+      // TODO: move raw tx out of file
+      String tx  =
+          '01000000019755f4f1def5f08d32ea2d43c9b46a6af38187266ee2520d5b1255b26462648f000000001976a914e3d4787f20cf11c0d10234bce832f99817c73d4888acffffffff0258020000000000001976a914a11b66a67b3ff69671c8f82254099faf374b800e88ac59810a00000000001976a914e3d4787f20cf11c0d10234bce832f99817c73d4888ac00000000';
+
+      BitcoinjsTransaction parsed = BitcoinjsTransaction.fromHex(tx);
+
+      expect (parsed.version, 1);
+      expect (parsed.hasWitnesses, false);
+
+      expect (parsed.ins.length,  1 );
+      expect (parsed.ins[0].index, 0 );
+      expect (parsed.ins[0].hash, "8f646264b255125b0d52e26e268781f36a6ab4c9432dea328df0f5def1f45597" );
+      expect (parsed.ins[0].script, "76a914e3d4787f20cf11c0d10234bce832f99817c73d4888ac" );
+      expect (parsed.ins[0].sequence, 4294967295 );
+
+
+
+    });
+  });
+  group('segwit', () {
+    test('parses unsigned', () {
+
+
+
+    String tx =
+        '02000000000101f44045600ea785218b4fff27d2224a6d26e88446ec201ab04eb089caaf691b5900000000160014bbfb0e0b6e264fef37aadf6b4f3f5c0fd997ed96ffffffff0258020000000000001976a914a11b66a67b3ff69671c8f82254099faf374b800e88ac61150f0000000000160014bbfb0e0b6e264fef37aadf6b4f3f5c0fd997ed9602000000000000';
+
+      });
+
+
+  });
+}
