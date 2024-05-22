0d9d274cb0808c6148b416545f7bccde9d45dd95
dime-squanderer <159725482+dime-squanderer@users.noreply.github.com>
2024-05-21 21:00:15 -0400

    cleanup format

--- lib/presentation/screens/onboarding_import/bloc/onboarding_import_bloc.dart
+++ lib/presentation/screens/onboarding_import/bloc/onboarding_import_bloc.dart
@@ -1,28 +1,38 @@
-import 'dart:developer';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:uniparty/common/uuid.dart';
 import 'package:uniparty/data/sources/local/dao/account_dao.dart';
 import 'package:uniparty/data/sources/local/db.dart';
-import 'package:uniparty/domain/entities/address.dart';
-import 'package:uniparty/domain/services/address_service.dart';
+
 import 'package:uniparty/presentation/screens/onboarding_import/bloc/onboarding_import_event.dart';
 import 'package:uniparty/presentation/screens/onboarding_import/bloc/onboarding_import_state.dart';
+import 'package:uniparty/domain/services/address_service.dart';
+import 'package:uniparty/domain/entities/address.dart';
 
-class OnboardingImportBloc extends Bloc<OnboardingImportEvent, OnboardingImportState> {
+import 'package:uniparty/common/constants.dart' as c;
+
+class OnboardingImportBloc
+    extends Bloc<OnboardingImportEvent, OnboardingImportState> {
   final addressService = GetIt.I<AddressService>();
-  final accountDao = GetIt.I<AccountDao>();
+    final accountDao = GetIt.I<AccountDao>();
+
   OnboardingImportBloc() : super(OnboardingImportState()) {
     on<MnemonicChanged>((event, emit) async {
       bool validMnemonic = true;
       if (validMnemonic) {
         try {
-          List<Address> addresses = await _deriveAddress(event.mnemonic, state.importFormat.name, addressService);
+          List<Address> addresses = await _deriveAddress(
+              event.mnemonic, state.importFormat.name, addressService);
 
-          emit(state.copyWith(mnemonic: event.mnemonic, getAddressesState: GetAddressesStateSuccess(addresses: addresses)));
+          emit(state.copyWith(
+              mnemonic: event.mnemonic,
+              getAddressesState:
+                  GetAddressesStateSuccess(addresses: addresses)));
         } catch (e) {
-          emit(state.copyWith(mnemonic: event.mnemonic, getAddressesState: GetAddressesStateError(message: e.toString())));
+          emit(state.copyWith(
+              mnemonic: event.mnemonic,
+              getAddressesState:
+                  GetAddressesStateError(message: e.toString())));
         }
       } else {
         emit(state.copyWith(mnemonic: event.mnemonic));
@@ -31,16 +41,24 @@ class OnboardingImportBloc extends Bloc<OnboardingImportEvent, OnboardingImportS
     on<ImportFormatChanged>((event, emit) async {
       bool validMnemonic = true;
 
-      ImportFormat importFormat = event.importFormat == "Segwit" ? ImportFormat.segwit : ImportFormat.freewalletBech32;
+      ImportFormat importFormat = event.importFormat == "Segwit"
+          ? ImportFormat.segwit
+          : ImportFormat.freewalletBech32;
 
       if (validMnemonic) {
         try {
-          List<Address> addresses = await _deriveAddress(state.mnemonic, importFormat.name, addressService);
+          List<Address> addresses = await _deriveAddress(
+              state.mnemonic, importFormat.name, addressService);
 
-          emit(
-              state.copyWith(importFormat: importFormat, getAddressesState: GetAddressesStateSuccess(addresses: addresses)));
+          emit(state.copyWith(
+              importFormat: importFormat,
+              getAddressesState:
+                  GetAddressesStateSuccess(addresses: addresses)));
         } catch (e) {
-          emit(state.copyWith(importFormat: importFormat, getAddressesState: GetAddressesStateError(message: e.toString())));
+          emit(state.copyWith(
+              importFormat: importFormat,
+              getAddressesState:
+                  GetAddressesStateError(message: e.toString())));
         }
       } else {
         emit(state.copyWith(importFormat: importFormat));
@@ -50,19 +68,25 @@ class OnboardingImportBloc extends Bloc<OnboardingImportEvent, OnboardingImportS
       final isCheckedMap = state.isCheckedMap;
       final nextMap = Map<Address, bool>.from(isCheckedMap);
       nextMap[event.address] = event.isChecked;
-      emit(state.copyWith(isCheckedMap: nextMap, importState: ImportStateNotAsked()));
+      emit(state.copyWith(
+          isCheckedMap: nextMap, importState: ImportStateNotAsked()));
     });
 
-    on<ImportAddresses>((event, emit) async {
+    on<ImportAddresses>((event, emit) async{
       // check if there are any address checked
       bool hasChecked = state.isCheckedMap.values.any((a) => a);
 
       if (!hasChecked) {
-        emit(state.copyWith(importState: ImportStateError(message: "Must select at least one address")));
+        emit(state.copyWith(
+            importState:
+                ImportStateError(message: "Must select at least one address")));
         return;
       } else {
-        List<Address> addresses =
-            state.isCheckedMap.entries.where((entry) => entry.value).map((entry) => entry.key).toList();
+
+        List<Address> addresses = state.isCheckedMap.entries
+            .where((entry) => entry.value)
+            .map((entry) => entry.key)
+            .toList();
 
         // create user account
         String accountUuid = uuid.v4();
@@ -72,17 +96,17 @@ class OnboardingImportBloc extends Bloc<OnboardingImportEvent, OnboardingImportS
 
         await accountDao.insertAccount(newAccount);
         Account? account = await accountDao.getAccountByUuid(accountUuid);
-        print('ACCOUNT>>>>> $account');
-        debugger(when: true);
-        // var account = accountDao.insertAccount(newAccount);
+        print('ACCOUNT: $account');
+
         // derive wallet from seed and save wif and public key, etc with user account
 
         // dervice keys for each address at path with wallet_id
+
         print(addresses);
 
         emit(state.copyWith(importState: ImportStateLoading()));
       }
-    });
+   });
   }
 }
 
@@ -121,16 +145,19 @@ class OnboardingImportBloc extends Bloc<OnboardingImportEvent, OnboardingImportS
 //   }
 // }
 
-_deriveAddress(String mnemonic, String importFormat, AddressService addressService) async {
+_deriveAddress(
+    String mnemonic, String importFormat, AddressService addressService) async {
   // TODO: swith on actual ENUM
   switch (importFormat) {
     // TODP
     case "Segwit":
       // TODO: obviously we should not be hardcoded to testnet
-      List<Address> addresses = await addressService.deriveAddressSegwitRange(mnemonic, 0, 7);
+      List<Address> addresses =
+          await addressService.deriveAddressSegwitRange(mnemonic, 0, 7);
       return addresses;
     case "Freewallet-bech32":
-      List<Address> addresses = await addressService.deriveAddressFreewalletBech32Range(mnemonic, 0, 7);
+      List<Address> addresses = await addressService
+          .deriveAddressFreewalletBech32Range(mnemonic, 0, 7);
       return addresses;
   }
 }
