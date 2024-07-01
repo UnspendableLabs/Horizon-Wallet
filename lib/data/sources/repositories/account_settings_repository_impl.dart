b5d621ccaf58de0e5998a7590568e03f47059ce2
matt marcello <matt.marcello@gmail.com>
2024-07-01 15:49:37 -0400

    address gen perf

--- lib/data/sources/repositories/account_settings_repository_impl.dart
+++ lib/data/sources/repositories/account_settings_repository_impl.dart
@@ -4,7 +4,7 @@ import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
 class AccountSettingsRepositoryImpl implements AccountSettingsRepository {
   final CacheProvider _cacheProvider;
-  final int _defaultGapLimit = 20;
+  final int _defaultGapLimit = 10;
 
   AccountSettingsRepositoryImpl({required CacheProvider cacheProvider})
       : _cacheProvider = cacheProvider;
--- lib/presentation/screens/addresses/bloc/addresses_bloc.dart
+++ lib/presentation/screens/addresses/bloc/addresses_bloc.dart
@@ -16,6 +16,9 @@ import 'package:horizon/remote_data_bloc/remote_data_state.dart';
 import 'package:horizon/domain/repositories/materialized_address_repository.dart';
 import 'package:horizon/domain/services/address_service.dart';
 import 'package:horizon/domain/services/encryption_service.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:rxdart/transformers.dart';
+import "dart:async";
 
 class AddressesBloc extends Bloc<AddressesEvent, AddressesState> {
   WalletRepository walletRepository;
@@ -33,24 +36,19 @@ class AddressesBloc extends Bloc<AddressesEvent, AddressesState> {
     required this.addressService,
     required this.encryptionService,
   }) : super(const AddressesState.initial()) {
-    on<Generate>((event, emit) async {
-      emit(const AddressesState.loading());
+    on<Generate>(
+      (event, emit) async {
+        emit(const AddressesState.loading());
 
-      try {
-        Account? account =
-            await accountRepository.getAccountByUuid(event.accountUuid);
+        try {
+          Account? account =
+              await accountRepository.getAccountByUuid(event.accountUuid);
 
-        if (account == null) {
-          throw Exception("invariant: account not found");
-        }
-
-        List<Address> addresses = [];
-
-        for (int i = 0; i < event.gapLimit; i++) {
-          if (_cache.containsKey((account, i))) {
-            addresses.add(_cache[(account, i)]!);
-            continue;
+          if (account == null) {
+            throw Exception("invariant: account not found");
           }
+
+          List<Address> addresses = [];
 
           Wallet? wallet = await walletRepository.getCurrentWallet();
 
@@ -61,28 +59,50 @@ class AddressesBloc extends Bloc<AddressesEvent, AddressesState> {
           String decryptedPrivKey = await encryptionService.decrypt(
               wallet.encryptedPrivKey, "UXGmJfeqoLXKGKk9tdk26hQvwIRpI6vm");
 
-          Address address = await addressService.deriveAddressSegwit(
-              privKey: decryptedPrivKey,
-              chainCodeHex: wallet.chainCodeHex,
-              accountUuid: account.uuid,
-              purpose: account.purpose,
-              coin: account.coinType,
-              account: account.accountIndex,
-              change: "0",
-              index: i);
+          for (int i = 0; i < event.gapLimit; i++) {
+            if (_cache.containsKey((account, i))) {
+              addresses.add(_cache[(account, i)]!);
+              continue;
+            }
 
-          _cache[(account, i)] = address;
+            Address address = await addressService.deriveAddressSegwit(
+                privKey: decryptedPrivKey,
+                chainCodeHex: wallet.chainCodeHex,
+                accountUuid: account.uuid,
+                purpose: account.purpose,
+                coin: account.coinType,
+                account: account.accountIndex,
+                change: "0",
+                index: i);
 
-          addresses.add(address);
+            _cache[(account, i)] = address;
+
+            addresses.add(address);
+          }
+
+          _data[account] = addresses;
+
+          emit(AddressesState.success(addresses));
+        } catch (e) {
+          emit(AddressesState.error(e.toString()));
         }
+      },
+      transformer: debounce(const Duration(milliseconds: 200)),
+    );
 
-
-        _data[account] = addresses;
-
-        emit(AddressesState.success(addresses));
-      } catch (e) {
-        emit(AddressesState.error(e.toString()));
-      }
-    });
   }
+}
+
+
+EventTransformer<Event> debounce<Event>(Duration duration) {
+  return (Stream<Event> events, Stream<Event> Function(Event) mapper) => events
+      .transform(
+        StreamTransformer<Event, Event>.fromHandlers(
+          handleData: (Event event, EventSink<Event> sink) => sink.add(event),
+          handleDone: (EventSink<Event> sink) => sink.close(),
+          handleError: (Object error, StackTrace stackTrace, EventSink<Event> sink) => sink.addError(error, stackTrace),
+        ),
+      )
+      .debounceTime(duration)
+      .switchMap(mapper);
 }
