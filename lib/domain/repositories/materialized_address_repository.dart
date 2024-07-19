79ad26dfddd72589206efa194d8bd8efe4191820
matt marcello <matt.marcello@gmail.com>
2024-07-19 13:56:53 -0400

    remove unused

--- lib/domain/repositories/materialized_address_repository.dart
+++
@@ -1,7 +0,0 @@
-import "package:horizon/domain/entities/address.dart";
-import "package:horizon/domain/entities/account.dart";
-
-abstract class MaterializedAddressRepository {
-  Future<List<Address>> getAddresses(Account account, int gapLimit,
-      [bool change = false]);
-}
