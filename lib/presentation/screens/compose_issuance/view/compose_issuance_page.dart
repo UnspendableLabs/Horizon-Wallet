d90b3fe6b5a30b2019c34f61c991f3e5ea7a3568
matt marcello <matt.marcello@gmail.com>
2024-08-19 12:31:11 -0400

    post rebase fix

--- lib/presentation/screens/compose_issuance/view/compose_issuance_page.dart
+++ lib/presentation/screens/compose_issuance/view/compose_issuance_page.dart
@@ -312,7 +312,8 @@ class _ComposeIssuancePageState extends State<_ComposeIssuancePage_> {
                             context
                                 .read<ComposeIssuanceBloc>()
                                 .add(CreateIssuanceEvent(
-                                  sourceAddress: fromAddress!,
+                                  sourceAddress:
+                                      fromAddress ?? addresses[0].address,
                                   password: passwordController.text,
                                   name: nameController.text,
                                   quantity:
