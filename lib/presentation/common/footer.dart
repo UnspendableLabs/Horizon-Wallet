e824443ecd8070eff4483cc4782460bc2b615833
matt marcello <matt.marcello@gmail.com>
2024-09-06 16:07:15 -0400

    privacy policy in footer

--- lib/presentation/common/footer.dart
+++ lib/presentation/common/footer.dart
@@ -5,29 +5,45 @@ import "package:horizon/presentation/colors.dart";
 class Footer extends StatelessWidget {
   const Footer({super.key});
 
+  Future<void> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      // Handle error
+      print('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 60,
       child: Center(
-        child: TextButton(
-          onPressed: () async {
-            const url =
-                'https://explorer.unspendablelabs.com/tos'; // Replace with your actual TOS URL
-            if (await canLaunch(url)) {
-              await launch(url);
-            } else {
-              // Handle error
-              print('Could not launch $url');
-            }
-          },
-          child: const Text(
-            'Terms of Service',
-            style: TextStyle(
-              color: neonBlueDarkThemeButtonTextColor,
-              fontSize: 16,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () => _launchURL('https://explorer.unspendablelabs.com/tos'),
+              child: const Text(
+                'Terms of Service',
+                style: TextStyle(
+                  color: neonBlueDarkThemeButtonTextColor,
+                  fontSize: 16,
+                ),
+              ),
             ),
-          ),
+            const SizedBox(width: 20), // Add some space between the links
+            TextButton(
+              onPressed: () => _launchURL('https://explorer.unspendablelabs.com/privacy-policy'),
+              child: const Text(
+                'Privacy Policy',
+                style: TextStyle(
+                  color: neonBlueDarkThemeButtonTextColor,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
