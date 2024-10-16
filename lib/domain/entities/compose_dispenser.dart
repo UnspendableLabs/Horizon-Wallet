a02b7dcd7ce0bc01df7b74892d03b8b9e40706ed
matt marcello <matt.marcello@gmail.com>
2024-10-16 10:44:24 -0400

    make btc change optional

--- lib/domain/entities/compose_dispenser.dart
+++ lib/domain/entities/compose_dispenser.dart
@@ -68,7 +68,7 @@ class ComposeDispenserResponseVerbose implements ComposeResponse {
   final String name;
   final int btcIn;
   final int btcOut;
-  final int btcChange;
+  final int? btcChange;
   final int btcFee;
   final String data;
 
@@ -78,7 +78,7 @@ class ComposeDispenserResponseVerbose implements ComposeResponse {
     required this.name,
     required this.btcIn,
     required this.btcOut,
-    required this.btcChange,
+     this.btcChange,
     required this.btcFee,
     required this.data,
   });
