b26308ddb5f1b10d69653f5521deb4152ca4fb75
dime-squanderer <159725482+dime-squanderer@users.noreply.github.com>
2024-10-18 14:42:21 -0400

    cleanup

--- lib/data/sources/repositories/config_repository_impl.dart
+++ lib/data/sources/repositories/config_repository_impl.dart
@@ -22,7 +22,7 @@ class ConfigImpl implements Config {
 
   String get _defaultCounterpartyApiBase => switch (network) {
         Network.mainnet => 'https://api.counterparty.io:4000/v2',
-        Network.testnet => 'http://api.counterparty.io:14000/v2',
+        Network.testnet => 'https://api.counterparty.io:14000/v2',
         Network.regtest => 'http://localhost:24000/v2'
       };
 
