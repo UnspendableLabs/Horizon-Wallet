a1d063ad75b3090450af8609361143403b2ee876
dime-squanderer <159725482+dime-squanderer@users.noreply.github.com>
2024-11-10 19:22:09 -0500

    fix named asset bug

--- lib/presentation/screens/compose_issuance/bloc/compose_issuance_event.dart
+++ lib/presentation/screens/compose_issuance/bloc/compose_issuance_event.dart
@@ -1,8 +1,3 @@
 import 'package:horizon/presentation/common/compose_base/bloc/compose_base_event.dart';
 
 abstract class ComposeIssuanceEvent extends ComposeBaseEvent {}
-
-class FetchBalances extends ComposeIssuanceEvent {
-  String address;
-  FetchBalances({required this.address});
-}
