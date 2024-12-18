e27dbb812e45179525e0078365654f90ce7bf655
dime-squanderer <159725482+dime-squanderer@users.noreply.github.com>
2024-12-17 21:54:59 -0500

    bump to 1.5

--- lib/data/sources/repositories/config_repository_impl.dart
+++ lib/data/sources/repositories/config_repository_impl.dart
@@ -3,7 +3,7 @@ import 'package:pub_semver/pub_semver.dart';
 
 class ConfigImpl implements Config {
   @override
-  Version get version => Version.parse('1.4.1');
+  Version get version => Version.parse('1.5.0');
 
   @override
   String get versionInfoEndpoint {
