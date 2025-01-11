1c264fddee427cb523e7cf2cb80b8e3a8f49ff0b
dime-squanderer <159725482+dime-squanderer@users.noreply.github.com>
2025-01-11 12:46:54 -0500

    fix asset name

--- lib/presentation/screens/update_issuance/bloc/update_issuance_bloc.dart
+++ lib/presentation/screens/update_issuance/bloc/update_issuance_bloc.dart
@@ -87,7 +87,7 @@ class UpdateIssuanceBloc extends ComposeBaseBloc<UpdateIssuanceState> {
     late FeeEstimates feeEstimates;
 
     try {
-      asset = await assetRepository.getAssetVerbose('A9999999999999990000');
+      asset = await assetRepository.getAssetVerbose(event.assetName!);
     } catch (e) {
       emit(state.copyWith(assetState: AssetState.error(e.toString())));
       return;
