9510e4f9589ac5c21af5fb7b38b05d208f527980
matt marcello <matt.marcello@gmail.com>
2025-01-29 12:24:48 -0500

    store imported key map on login

--- lib/data/sources/repositories/in_memory_key_repository_impl.dart
+++ lib/data/sources/repositories/in_memory_key_repository_impl.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'package:horizon/domain/services/secure_kv_service.dart';
 import 'package:horizon/domain/repositories/in_memory_key_repository.dart';
 
 class InMemoryKeyRepositoryImpl implements InMemoryKeyRepository {
   final _key = 'DEFAULT_KEY';
+  final _mapKey = 'IMPORTED_KEY_MAP';
 
   final SecureKVService secureKVService;
 
@@ -19,7 +21,27 @@ class InMemoryKeyRepositoryImpl implements InMemoryKeyRepository {
   }
 
   @override
+  Future<void> setMap({required Map<String, String> map}) async {
+    final json = map.toString();
+    return await secureKVService.write(key: _mapKey, value: json);
+  }
+
+  @override
+  Future<Map<String, String>> getMap() async {
+    try {
+      String? string = await secureKVService.read(key: _mapKey);
+
+      return json.decode(string!).map((key, value) {
+        return MapEntry(key, value as String);
+      });
+    } catch (e) {
+      return {};
+    }
+  }
+
+  @override
   Future<void> delete() async {
-    return await secureKVService.delete(key: _key);
+    await secureKVService.delete(key: _key);
+    await secureKVService.delete(key: _mapKey);
   }
 }
