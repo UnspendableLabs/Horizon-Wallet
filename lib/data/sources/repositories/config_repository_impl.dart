4f81bacc1251df0a928ffada6a115f60d5eab860
dime-squanderer <159725482+dime-squanderer@users.noreply.github.com>
2025-02-13 10:10:26 -0500

    bump version

--- lib/data/sources/repositories/config_repository_impl.dart
+++ lib/data/sources/repositories/config_repository_impl.dart
@@ -3,7 +3,7 @@ import 'package:pub_semver/pub_semver.dart';
 
 class ConfigImpl implements Config {
   @override
-  Version get version => Version.parse('1.7.3');
+  Version get version => Version.parse('1.7.4');
 
   @override
   String get versionInfoEndpoint {
