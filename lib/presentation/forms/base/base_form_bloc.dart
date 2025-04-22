bcd6a227b477484a97d01ff14c05e16506212051
matt marcello <matt.marcello@gmail.com>
2025-04-22 15:04:29 -0400

    wip

---
+++ lib/presentation/forms/base/base_form_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+import './base_form_state.dart';
+import './base_form_event.dart';
+
+abstract class Loader<Args, Data> {
+  Future<Data> load(Args args);
+}
+
+class BaseFormBloc<LoadArgs, LoadData>
+    extends Bloc<BaseFormEvent, BaseFormState<LoadData>> {
+  Loader<LoadArgs, LoadData> _loader;
+
+  BaseFormBloc({required Loader<LoadArgs, LoadData> loader})
+      : _loader = loader,
+        super(Initial<LoadData>()) {
+    on<LoadDependencies<LoadArgs>>(_loadWithArgs);
+  }
+
+  Future<void> load(
+    LoadArgs args,
+  ) async {
+    add(LoadDependencies<LoadArgs>(args));
+  }
+
+  Future<void> _loadWithArgs(
+    LoadDependencies<LoadArgs> event,
+    Emitter<BaseFormState> emit,
+  ) async {
+    emit(Loading<LoadData>());
+
+    try {
+      final formDeps = await _loader.load(event.args);
+      emit(Success<LoadData>(formDeps));
+    } catch (e) {
+      emit(Failure<LoadData>(e));
+    }
+  }
+}
--- lib/presentation/screens/transactions/send/view/send_page.dart
+++ lib/presentation/screens/transactions/send/view/send_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:horizon/presentation/forms/send_form/send_form_view.dart';
+import 'package:horizon/presentation/forms/send_form_refactor/send_form_view.dart';
 import 'package:horizon/presentation/screens/transactions/send/bloc/send_page_bloc.dart';
 
 class SendPage extends StatefulWidget {
@@ -19,14 +19,10 @@ class SendPage extends StatefulWidget {
 class _SendPageState extends State<SendPage> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SendPageBloc(
-        const SendPageState(),
-      ),
-      child: SendFormView(
-        addresses: widget.addresses,  
-        assetName: widget.assetName,
-      ),
+    return SendFormProvider(
+      addresses: widget.addresses,
+      assetName: widget.assetName,
     );
   }
 }
+
