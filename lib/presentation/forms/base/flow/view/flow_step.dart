52ec982ae2ae6405053785e7769968506c5e5e5e
matt marcello <matt.marcello@gmail.com>
2025-09-29 11:51:29 -0400

    move title to app bar

--- lib/presentation/forms/base/flow/view/flow_step.dart
+++ lib/presentation/forms/base/flow/view/flow_step.dart
@@ -24,6 +24,12 @@ class FlowStep extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Theme.of(context).scaffoldBackgroundColor,
           elevation: 0,
+          centerTitle: true,
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.titleMedium,
+            textAlign: TextAlign.center,
+          ),
           leading: leading,
           automaticallyImplyLeading: leading != null,
           actions: trailing != null ? [trailing!] : null,
@@ -73,15 +79,6 @@ class FlowStep extends StatelessWidget {
                 const SizedBox(height: 16),
 
                 // Step title
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: isSmallScreen ? 16 : 30),
-                  child: Text(
-                    title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
                 const SizedBox(height: 24),
 
                 // Main content
