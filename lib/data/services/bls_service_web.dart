70648ca94eb9739955f36ece000a89262b90d2e1
Ouziel Slama <ouziel@gmail.com>
2026-02-18 14:12:53 +0100

    fix import

--- lib/data/services/bls_service_web.dart
+++ lib/data/services/bls_service_web.dart
@@ -1,5 +1,6 @@
 import 'dart:convert' show utf8;
 import 'dart:js_interop';
+import 'dart:typed_data';
 import 'package:convert/convert.dart' as convert;
 import 'package:horizon/domain/services/bls_service.dart';
 import 'package:horizon/js/bls.dart' as bls;
